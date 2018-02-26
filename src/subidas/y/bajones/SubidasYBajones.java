bee2a48be89978a98a53b3e42e7a6edb24364c8d
Aragon Perez <Aragon Perez@Selvin>
2018-02-26 15:42:04 -0600

    Practica1_Inicializada

---
+++ src/subidas/y/bajones/SubidasYBajones.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package subidas.y.bajones;
+
+/**
+ *
+ * @author Aragon Perez
+ */
+public class SubidasYBajones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        while(true){
+            System.out.println("[IPC1]Tarea3_201701133");
+            System.out.println("  ");
+            System.out.println("1. Dificultad del Juego");
+            System.out.println("2. Parametros Inciales");
+            System.out.println("3. Inciar Juego");
+            System.out.println("4. Salir");
+        }
+    }
+    
+}
